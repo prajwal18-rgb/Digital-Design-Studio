ddb50acbbf76efc4ec4d2f6b1215951f17c5cdb4
prajwal18-rgb <prajwal9d@gmail.com>
2026-05-02 15:51:58 +0530

    Update [trpc].ts

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -2,7 +2,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../router";
 import { createContext } from "../context";
 
-export const config = { runtime: "nodejs20.x" };
+export const config = { runtime: "nodejs" };
 
 export default function handler(req: Request) {
   return fetchRequestHandler({
