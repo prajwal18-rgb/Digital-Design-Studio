77fc87b7f49f69e6bd57b4615652a2dda191866c
prajwal18-rgb <prajwal9d@gmail.com>
2026-05-03 00:17:05 +0530

    Update supabase.ts
    fuck a long huge error is being solved this instant

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://hzogtqskgzbtvknrojos.supabase.co'
-const supabaseKey = 'sb_publishable_aY44_wl0JJmzTqVEhhMnPQ__C8TaIW7'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6b2d0cXNrZ3pidHZrbnJvam9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MzQ0ODUsImV4cCI6MjA5MzIxMDQ4NX0.V9PHfIYa-7pVyOJwPNY05125YfktCSg4zBN13DZM5ug'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
